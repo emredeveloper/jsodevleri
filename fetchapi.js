1c0f4e3ae100c6e7a34c2d247094e049fc1220ad
emredeveloper <karatasqemre@gmail.com>
2023-02-11 23:22:19 +0300

    guncelleme ve ekleme

---
+++ fetchapi.js
@@ -0,0 +1,11 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=>response.json())
+.then((json)=>console.log(json))
+.catch((err)=>console.log(err));
+
+
+
+fetch("https://jsonplaceholder.typicode.com/comments")
+.then((response) => response.json())
+.then((json) => json.forEach((item) => console.log(item.email)))
+.catch((err) => console.log(err))
